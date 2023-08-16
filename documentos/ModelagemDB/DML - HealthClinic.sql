9dfd578c4fe9979d2fad65a61d80fb4f7f2e20aa
Guilherme Sousa Oliveira <guilhermesousa1110@gmail.com>
2023-08-16 10:57:13 -0300

    Modelagem de dados

--- documentos/ModelagemDB/DML - HealthClinic.sql
+++ documentos/ModelagemDB/DML - HealthClinic.sql
@@ -11,4 +11,24 @@ INSERT INTO TB_PlanoSaude (NomePlanoSaude)
 VALUES ('Sulamérica'),('Porto Seguro'),('Unimed'),('NotreDame')
 
 INSERT INTO TB_Prontuario (Descricao)
-VALUES ('Primeira consulta')
+VALUES ('Primeira consulta do paciente.')
+
+INSERT INTO TB_Usuario (IdTipoDeUsuario, NomeCompleto, Email, Senha, DataNascimento, CPF, Sexo)
+VALUES (1, 'Guilherme Sousa Oliveira', 'guilherme@email.com', 'guilherme123', '2002-04-26', '12345678907', 0),
+		(2, 'Ana Beatriz Soares', 'ana@email.com', 'ana123', '1998-06-11',  '7654321098', 1),
+		(3, 'Eder Roberto Lopes', 'eder@email.com', 'eder123', '2002-04-26', '8989878765', 0)
+
+INSERT INTO TB_Medico (IdClinica, IdUsuario,IdEspecialidade, CRM)
+VALUES(1, 2, 3, '1234SP')
+
+INSERT INTO TB_Adm (IdUsuario, Cargo)
+VALUES(1, 'Secretário')
+
+INSERT INTO TB_Paciente (IdUsuario, IdPlanoSaude, IdProntuario, Endereco)
+VALUES (3, 1, 1, 'Rua cara e coroa, 193')
+
+INSERT INTO TB_Feedback (IdPaciente, Descricao)
+VALUES(1, 'Muito boa a consulta!')
+
+INSERT INTO TB_Consulta(IdAdm, IdPaciente, IdMedico, IdFeedback, DataConsulta, HoraConsulta, DescricaoConsulta)
+VALUES(1, 1, 1, 1, '2023-08-15', '09:30:00', 'Paciente com muitas dores de cabeça')
--- documentos/ModelagemDB/DQL - HealthClinic.sql
+++ documentos/ModelagemDB/DQL - HealthClinic.sql
@@ -1,5 +1,39 @@
-SELECT * FROM TB_TipoDeUsuario
-SELECT * FROM TB_Especialidade
-SELECT * FROM TB_Clinica
-SELECT * FROM TB_PlanoSaude
-SELECT * FROM TB_Prontuario
+
+
+SELECT
+	TB_Consulta.IdConsulta,
+	TB_Consulta.DataConsulta,
+	TB_Consulta.HoraConsulta,
+	TB_Clinica.NomeFantasia,
+	TB_Usuario.NomeCompleto,
+	TB_Usuario.NomeCompleto,
+	TB_Medico.CRM,
+	TB_Prontuario.Descricao,
+	TB_Feedback.Descricao
+FROM
+	TB_Consulta
+	INNER JOIN
+	TB_Medico ON TB_Consulta.IdMedico = TB_Medico.IdMedico
+	INNER JOIN
+	TB_Clinica ON TB_Medico.IdClinica = TB_Clinica.IdClinica
+	INNER JOIN
+	TB_Paciente ON TB_Paciente.IdPaciente = TB_Consulta.IdPaciente
+	INNER JOIN
+	TB_Usuario ON TB_Usuario.IdUsuario = TB_Medico.IdUsuario OR TB_Usuario.IdUsuario = TB_Paciente.IdUsuario
+	INNER JOIN
+	TB_Prontuario ON TB_Prontuario.IdProntuario = TB_Paciente.IdProntuario
+	INNER JOIN
+	TB_Feedback ON TB_Feedback.IdPaciente = TB_Paciente.IdPaciente
+
+
+	SELECT NomeCompleto,TB_Medico.IdMedico,IdConsulta FROM TB_Usuario
+	INNER JOIN TB_Paciente
+	ON TB_Paciente.IdUsuario = TB_Usuario.IdUsuario
+	INNER JOIN TB_Consulta
+	ON TB_Consulta.IdPaciente = TB_Paciente.IdPaciente
+	INNER JOIN TB_Medico
+	ON  TB_Consulta.IdMedico =  TB_Medico.IdMedico
+	WHERE TB_Paciente.IdUsuario = TB_Usuario.IdUsuario
+
+
+
